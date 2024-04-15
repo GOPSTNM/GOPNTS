1c3b758e4f8eb9755dbe35e4a3126300a50a8fb0
GOPSTNM <166233545+GOPSTNM@users.noreply.github.com>
2024-04-15 08:40:58 +0800

    Update nav.js

--- nav.js
+++ nav.js
@@ -3,6 +3,6 @@ fetch('navbar.html')
 .then(text => {
     let oldelem = document.querySelector("script#replace_with_navbar");
     let newelem = document.createElement("div");
-    newelem.innerHTML = text + page;
+    newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem,oldelem);
 })
