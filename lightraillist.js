afbc0e48eac08177b924556557a5dc8badb34afc
GOPSTNM <166233545+GOPSTNM@users.noreply.github.com>
2024-04-15 21:11:15 +0800

    Update lightraillist.js

--- lightraillist.js
+++ lightraillist.js
@@ -1,3 +1,3 @@
-const Z1 = ["Tuen Mun Ferry Pier 屯門碼頭", "Melody Garden 美樂", "Butterfly 蝴蝶", "Light Rail Depot 輕鐵車廠", "Lung Mun 龍門",
+const zoneone = ["Tuen Mun Ferry Pier 屯門碼頭", "Melody Garden 美樂", "Butterfly 蝴蝶", "Light Rail Depot 輕鐵車廠", "Lung Mun 龍門",
            "hi"];
-alert(Z1[1]);
+alert(zoneone[1]);
