831e115b6ab9fdca7d4cf24ed24fdd59e6f6c871
GOPSTNM <166233545+GOPSTNM@users.noreply.github.com>
2024-04-18 21:11:45 +0800

    Update lightraillist.js

--- lightraillist.js
+++ lightraillist.js
@@ -1,13 +1,15 @@
-const zone1 = ["Tuen Mun Ferry Pier 屯門碼頭", "Melody Garden 美樂", "Butterfly 蝴蝶",
+var lrstop = { zone1 = ["Tuen Mun Ferry Pier 屯門碼頭", "Melody Garden 美樂", "Butterfly 蝴蝶",
                "Light Rail Depot 輕鐵車廠", "Lung Mun 龍門", "Siu Hei 兆禧",
                "Tuen Mun Swimming Pool 屯門泳池", "Goodview Garden 豐景園",
-               "Siu Lun 兆麟", "Sam Shing 三聖"];
-const zone1data = [1, 10, 15, 20, 30, 240, 250, 260, 265, 920];
-const zone2 = ["Tsing Shan Tsuen 青山村"];
-const zone2data = [40];
+               "Siu Lun 兆麟", "Sam Shing 三聖"], 
+              zone1data = [1, 10, 15, 20, 30, 240, 250, 260, 265, 920], 
+              zone2 = ["Tsing Shan Tsuen 青山村"], 
+              zone2data = [40]
+             };
 function zonechange() {
-  alert(zone1.toString());
-  alert(document.getElementById("lightrailzone").value);
+  var currzone = document.getElementById("lightrailzone").value;
+  alert(currzone);
+  alert(lrstop[currzone]);
 }
 function write(cont) {
   document.getElementById("lightrailstop").innerHTML = cont;
