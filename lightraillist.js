fac2dfab782e774beb19cd43f9bd96c3ec79836f
GOPSTNM <166233545+GOPSTNM@users.noreply.github.com>
2024-04-18 17:25:08 +0800

    Update lightraillist.js

--- lightraillist.js
+++ lightraillist.js
@@ -6,7 +6,7 @@ const zone1data = [1, 10, 15, 20, 30, 240, 250, 260, 265, 920];
 const zone2 = ["Tsing Shan Tsuen 青山村"];
 const zone2data = [40];
 function zonechange() {
-  alert(zone1);
+  alert(zone1.toString());
 }
 function write(cont) {
   document.getElementById("lightrailstop").innerHTML = cont;
